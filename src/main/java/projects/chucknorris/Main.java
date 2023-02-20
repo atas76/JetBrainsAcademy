628fd1084c052970579f5d43fe273e65f0a4c39c
atas <andreas.tasoulas@gmail.com>
2023-02-21 01:16:16 +0200

    Chuck Norris Cipher Encoder stage 3: encrypt with zeros only

--- src/main/java/projects/chucknorris/Main.java
+++ src/main/java/projects/chucknorris/Main.java
@@ -10,8 +10,58 @@ public class Main {
 
         System.out.println("Input string:");
         String input = scanner.nextLine();
+        String binaryString = getBinaryString(input);
 
         System.out.println("The result:");
+    }
+
+    static String getChuckNorrisUnaryCode(String inputString) {
+
+        String binaryString = getBinaryString(inputString);
+        char currentBit = binaryString.charAt(0);
+        int currentSequenceCount = 0;
+        int index = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while (index < binaryString.length() - 1) {
+            while (binaryString.charAt(index) == currentBit) {
+                ++currentSequenceCount;
+                ++index;
+                if (index > binaryString.length() - 1) break;
+            }
+            sb.append(encode(currentBit, currentSequenceCount));
+            if (index > binaryString.length() - 1) break;
+            sb.append(" ");
+            currentBit = binaryString.charAt(index);
+            currentSequenceCount = 0;
+        }
+        return sb.toString();
+    }
+
+    static String encode(char bit, int sequenceCount) {
+
+        StringBuilder sb = new StringBuilder();
+
+        if (bit == '1') {
+            sb.append("0 ");
+        } else {
+            sb.append("00 ");
+        }
+        for (int i = 0; i < sequenceCount; i++) {
+            sb.append("0");
+        }
+
+        return sb.toString();
+    }
+
+    private static String getBinaryString(String input) {
+        StringBuilder sb = new StringBuilder();
+        input.chars().mapToObj(c -> (char) c).forEach(ch ->
+                sb.append(String.format("%7s", Integer.toBinaryString(ch)).replace(' ', '0')));
+        return sb.toString();
+    }
+
+    private static void printBinary(String input) {
         input.chars().forEach(ch -> System.out.println((char) ch + " = " +
                 String.format("%7s", Integer.toBinaryString(ch)).replace(' ', '0')));
     }
---
+++ src/test/java/projects/chucknorris/MainTest.java
@@ -0,0 +1,45 @@
+package projects.chucknorris;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    public void testEncodeCharacter() {
+        assertEquals("0 0 00 0000 0 00", Main.getChuckNorrisUnaryCode("C"));
+    }
+
+    @Test
+    public void testEncodeTwoCharacters() {
+        assertEquals("0 0 00 0000 0 000 00 0000 0 00", Main.getChuckNorrisUnaryCode("CC"));
+    }
+
+    @Test
+    public void testEncodeString() {
+        assertEquals("0 0 00 00 0 0 00 000 0 00 00 0 0 0 00 00 0 0 00 0 0 0 00 000000 0 0000 00 000 0 00 00 00 0 00",
+                Main.getChuckNorrisUnaryCode("Hi <3"));
+    }
+
+    @Test
+    public void testEncodeSingleOne() {
+        String encodedString = Main.encode('1', 1);
+
+        assertEquals("0 0", encodedString);
+    }
+
+    @Test
+    public void testEncodeFourZeros() {
+        String encodedString = Main.encode('0', 4);
+
+        assertEquals("00 0000", encodedString);
+    }
+
+    @Test
+    public void testEncodeTwoOnes() {
+        String encodedString = Main.encode('1', 2);
+
+        assertEquals("0 00", encodedString);
+    }
+}
