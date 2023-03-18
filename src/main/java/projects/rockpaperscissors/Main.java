83f78b3a415382d67ffe562f11b84862a9b8b3f0
atas <andreas.tasoulas@gmail.com>
2023-03-18 03:59:22 +0200

    Rock-Paper-Scissors stage 4: Scoring the game

--- src/main/java/projects/rockpaperscissors/Main.java
+++ src/main/java/projects/rockpaperscissors/Main.java
@@ -1,9 +1,9 @@
 package projects.rockpaperscissors;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Random;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class Main {
 
@@ -27,23 +27,67 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Map<String, Integer> scores = readScores();
+
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String playerName = scanner.next();
+        int playerScore = scores.getOrDefault(playerName, 0);
+
+        System.out.println("Hello, " + playerName);
 
         String playerChoice = scanner.next();
 
         while (!"!exit".equals(playerChoice)) {
             String currentPlayerChoice = playerChoice;
+            if (playerChoice.startsWith("!")) {
+                if ("!rating".equals(playerChoice)) { // To be replaced with switch in future iterations
+                    System.out.println("Your rating: " + playerScore);
+                } else {
+                    System.out.println("Invalid input");
+                }
+                playerChoice = scanner.next();
+                continue;
+            }
             if (Arrays.stream(CHOICES).noneMatch(choice -> choice.equals(currentPlayerChoice))) {
                 System.out.println("Invalid input");
                 playerChoice = scanner.next();
                 continue;
             }
             String computerChoice = getComputerChoice(playerChoice);
-            displayResult(compare(playerChoice, computerChoice), computerChoice);
+            int result = compare(playerChoice, computerChoice);
+            playerScore += 50 + result * 50;
+            displayResult(result, computerChoice);
             playerChoice = scanner.next();
         }
 
         System.out.println("Bye!");
+
+    }
+
+    private static Map<String, Integer> readScores() {
+        Map<String, Integer> scores = new HashMap<>();
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("rating.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String [] currentRecord = line.split("\s");
+                scores.put(currentRecord[0], Integer.valueOf(currentRecord[1]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return scores;
     }
 
     private static void displayResult(int result, String computerOption) {
