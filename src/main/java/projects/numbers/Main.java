637794ce8ff2a87a0471d8625c9170b99a33b4ad
atas <andreas.tasoulas@gmail.com>
2023-04-09 01:02:17 +0300

    Solve buzz numbers arithmeticallly

--- src/main/java/projects/numbers/Main.java
+++ src/main/java/projects/numbers/Main.java
@@ -9,26 +9,25 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter a natural number: ");
-        String numberInput = scanner.next();
-        int number = Integer.parseInt(numberInput);
+        int number = scanner.nextInt();
         if (number > 0) {
             String parity = number % 2 == 0 ? "Even" : "Odd";
             boolean divisibleBySeven = (number % 7 == 0);
-            boolean lastDigitSeven = numberInput.endsWith("7");
+            boolean lastDigitSeven = number % 10 == 7;
             boolean isBuzz = divisibleBySeven || lastDigitSeven;
 
             System.out.println("This number is " + parity + ".");
             System.out.println("It is " + (isBuzz ? "" : "not ") + "a Buzz number.");
             System.out.println("Explanation:");
             if (!isBuzz) {
-                System.out.println(numberInput + " is neither divisible by 7 nor does it end with 7.");
+                System.out.println(number + " is neither divisible by 7 nor does it end with 7.");
             } else {
                 if (divisibleBySeven && !lastDigitSeven) {
-                    System.out.println(numberInput + " is divisible by 7.");
+                    System.out.println(number + " is divisible by 7.");
                 } else if (!divisibleBySeven) {
-                    System.out.println(numberInput + " ends with 7.");
+                    System.out.println(number + " ends with 7.");
                 } else {
-                    System.out.println(numberInput + " is divisible by 7 and ends with 7.");
+                    System.out.println(number + " is divisible by 7 and ends with 7.");
                 }
             }
         } else {
