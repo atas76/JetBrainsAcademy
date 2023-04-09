be17d8ebea78620a4463afaa8a6b52dd588a4ccb
atas <andreas.tasoulas@gmail.com>
2023-04-10 02:44:56 +0300

    Amazing numbers stage 4: refactor to support multiple numbers per input

--- src/main/java/projects/numbers/Main.java
+++ src/main/java/projects/numbers/Main.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static String input;
+    private static String [] inputArray;
+    private static int inputOffset;
+    private static long numberInput;
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -11,23 +16,51 @@ public class Main {
         System.out.println("Welcome to Amazing Numbers!");
         System.out.println("Supported requests:");
         System.out.println("- enter a natural number to know its properties;");
+        System.out.println("enter two natural numbers to obtain the properties of the list:");
+        System.out.println("* the first parameter represents a starting number;");
+        System.out.println("* the second parameter shows how many consecutive numbers are to be processed;");
+        System.out.println("- separate the parameters with one space;");
         System.out.println("- enter 0 to exit.");
         System.out.println();
-        System.out.println("Enter a request: ");
+        System.out.println("Enter a request: "); readInput(scanner);
 
-        long numberInput = scanner.nextLong();
         while (numberInput != 0) {
-            if (numberInput > 0) {
-                new Number(numberInput).print();
-            } else {
-                System.out.println("The first parameter should be a natural number or zero.");
+            if (inputOffset == 0) {
+                if (numberInput > 0) {
+                    new Number(numberInput).print();
+                } else {
+                    System.out.println("The first parameter should be a natural number or zero.");
+                }
+            }
+            if (inputOffset > 0) {
+                if (numberInput > 0) {
+                    for (long i = numberInput; i < numberInput + inputOffset; i++) {
+                        new Number(i).printSummary();
+                    }
+                } else {
+                    System.out.println("The first parameter should be a natural number or zero.");
+                }
             }
             System.out.println();
-            System.out.println("Enter a request: ");
-            numberInput = scanner.nextLong();
+            System.out.println("Enter a request: "); readInput(scanner);
         }
 
         System.out.println();
         System.out.println("Goodbye!");
+    }
+
+    private static void readInput(Scanner scanner) {
+        input = scanner.nextLine();
+        inputArray = input.split("\s");
+        inputOffset = 0;
+        try {
+            numberInput = Long.parseLong(inputArray[0]);
+        } catch(NumberFormatException nfex) {
+            numberInput = -1;
+            return;
+        }
+        if (inputArray.length == 2) {
+            inputOffset = Integer.parseInt(inputArray[1]);
+        }
     }
 }
--- src/main/java/projects/numbers/Number.java
+++ src/main/java/projects/numbers/Number.java
@@ -1,35 +1,44 @@
 package projects.numbers;
 
+import java.util.*;
+
 public class Number {
 
     private long number;
-    private boolean even;
-    private boolean odd;
-    private boolean buzz;
-    private boolean duck;
-    private boolean palindromic;
+
+    private Map<String, Boolean> properties = new TreeMap<>();
 
     public Number(long number) {
 
         this.number = number;
 
-        even = number % 2 == 0;
-        odd = !even;
+        properties.put("even", number % 2 == 0) ;
+        properties.put("odd", number % 2 != 0);
 
         boolean divisibleBySeven = (number % 7 == 0);
         boolean lastDigitSeven = number % 10 == 7;
-        this.buzz = divisibleBySeven || lastDigitSeven;
-        this.duck = isDuck();
-        this.palindromic = isPalindromic();
+        properties.put("buzz", divisibleBySeven || lastDigitSeven);
+
+        properties.put("duck", isDuck());
+        properties.put("palindromic", isPalindromic());
     }
 
     public void print() {
         System.out.println("Properties of " + number);
-        System.out.println("even: " + even);
-        System.out.println("odd: " + odd);
-        System.out.println("buzz: " + buzz);
-        System.out.println("duck: " + duck);
-        System.out.println("palindromic: " + palindromic);
+        System.out.println("even: " + properties.get("even"));
+        System.out.println("odd: " + properties.get("odd"));
+        System.out.println("buzz: " + properties.get("buzz"));
+        System.out.println("duck: " + properties.get("duck"));
+        System.out.println("palindromic: " + properties.get("palindromic"));
+    }
+
+    public void printSummary() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(number);
+        sb.append(" is ");
+        properties.forEach((property, value) -> { if (properties.get(property)) sb.append(property).append(", "); });
+
+        System.out.println(sb.substring(0, sb.length() - 2));
     }
 
     private boolean isDuck() {
