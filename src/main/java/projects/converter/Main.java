6a063d02282a836746a35ed2a2b415f436ff0e6f
atas <andreas.tasoulas@gmail.com>
2023-05-23 22:20:57 +0300

    Number base converter stage 2 (1/n): enter input in a loop

--- src/main/java/projects/converter/Main.java
+++ src/main/java/projects/converter/Main.java
@@ -6,21 +6,38 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        StringBuilder result = new StringBuilder();
 
-        System.out.println("Enter number in decimal system: ");
-        int decimalNumber = scanner.nextInt();
 
-        System.out.println("Enter target base: ");
-        int targetBase = scanner.nextInt();
+        String command;
 
-        int quotient = decimalNumber;
+        do {
 
-        while (quotient != 0) {
-            result.append(Character.forDigit(quotient % targetBase, targetBase));
-            quotient /= targetBase;
-        }
+            System.out.println("Do you want to convert /from decimal or /to decimal? (To quit type /exit)");
+            command = scanner.next();
 
-        System.out.println("Conversion result: " + result.reverse());
+            if ("/from".equals(command)) {
+
+                StringBuilder result = new StringBuilder();
+
+                System.out.println("Enter number in decimal system: ");
+                int decimalNumber = scanner.nextInt();
+
+                System.out.println("Enter target base: ");
+                int targetBase = scanner.nextInt();
+
+                int quotient = decimalNumber;
+
+                while (quotient != 0) {
+                    result.append(Character.forDigit(quotient % targetBase, targetBase));
+                    quotient /= targetBase;
+                }
+
+                System.out.println("Conversion result: " + result.reverse());
+                System.out.println();
+            }
+            if ("/to".equals(command)) {
+
+            }
+        } while (!"/exit".equals(command));
     }
 }
