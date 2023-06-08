aff2889f69399b235dcff609e913281f372c9e90
atas <andreas.tasoulas@gmail.com>
2023-06-08 22:45:41 +0300

    Number base converter stage 4: convert fractions (basic implementation)

--- src/main/java/projects/converter/Main.java
+++ src/main/java/projects/converter/Main.java
@@ -1,6 +1,8 @@
 package projects.converter;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class Main {
@@ -21,11 +23,11 @@ public class Main {
 
             System.out.println("Enter number in base " + sourceBase + " to convert to base " +
                     targetBase + " (To go back type /back)");
-            String number = scanner.next();
-            if ("/exit".equals(number)) {
+            String numberInput = scanner.next();
+            if ("/exit".equals(numberInput)) {
                 break;
             }
-            if ("/back".equals(number)) {
+            if ("/back".equals(numberInput)) {
                 System.out.println("Enter two numbers in format: {source base} {target base} (To quit type /exit)");
                 input = scanner.next();
                 if ("/exit".equals(input)) {
@@ -36,22 +38,49 @@ public class Main {
                 continue;
             }
 
-            BigInteger result = BigInteger.ZERO;
+            BigDecimal result = BigDecimal.ZERO;
+
+            String integerPart = numberInput;
+            String fractionalPart = "";
+            String convertedFractionalPart = "";
+            if (numberInput.contains(".")) {
+                String [] numberParts = numberInput.split("\\.");
+                integerPart = numberParts[0];
+                fractionalPart = numberParts[1];
+            }
 
             if (sourceBase == 10) {
-                BigInteger decimalNumber = new BigInteger(number);
-                System.out.println("Conversion result: " + convertFromDec(targetBase, decimalNumber));
+                BigInteger decimalNumberIntegerPart = new BigInteger(integerPart);
+                if (!fractionalPart.isEmpty()) {
+                    fractionalPart = "0." + fractionalPart;
+                    BigDecimal decimalNumberFractionalPart = new BigDecimal(fractionalPart);
+                    convertedFractionalPart = convertFromDecFractionalPart(targetBase, decimalNumberFractionalPart);
+                }
+                System.out.println("Conversion result: " +
+                        convertFromDecIntegerPart(targetBase, decimalNumberIntegerPart) +
+                                (!convertedFractionalPart.isEmpty() ? "." + convertedFractionalPart : ""));
                 System.out.println();
             } else if (sourceBase < 10) {
-                for (int i = 0; i < number.length(); i++) {
-                    double currentDigit = (number.charAt(i) - '0') * Math.pow(sourceBase, number.length() - i - 1);
-                    result = result.add(BigInteger.valueOf((long) currentDigit));
+                for (int i = 0; i < integerPart.length(); i++) {
+                    double currentDigit = (integerPart.charAt(i) - '0') * Math.pow(sourceBase, integerPart.length() - i - 1);
+                    result = result.add(BigDecimal.valueOf((long) currentDigit));
+                }
+                for (int i = 0; i < fractionalPart.length(); i++) {
+                    double currentDigit = (fractionalPart.charAt(i) - '0') / Math.pow(sourceBase, i + 1);
+                    result = result.add(BigDecimal.valueOf((long) currentDigit));
+                    result = result.setScale(5, RoundingMode.HALF_UP);
                 }
             } else {
-                for (int i = 0; i < number.length(); i++) {
-                    double currentDigit = convertFromAlphabeticalDigit(number.charAt(i)) *
-                            Math.pow(sourceBase, number.length() - i - 1);
-                    result = result.add(BigInteger.valueOf((long) currentDigit));
+                for (int i = 0; i < integerPart.length(); i++) {
+                    double currentDigit = convertFromAlphabeticalDigit(integerPart.charAt(i)) *
+                            Math.pow(sourceBase, integerPart.length() - i - 1);
+                    result = result.add(BigDecimal.valueOf((long) currentDigit));
+                }
+                for (int i = 0; i < fractionalPart.length(); i++) {
+                    double currentDigit = convertFromAlphabeticalDigit(fractionalPart.charAt(i)) /
+                            Math.pow(sourceBase, i + 1);
+                    result = result.add(BigDecimal.valueOf(currentDigit));
+                    result = result.setScale(5, RoundingMode.HALF_UP);
                 }
             }
             if (sourceBase != 10) {
@@ -59,14 +88,39 @@ public class Main {
                     System.out.println("Conversion result: " + result);
                     System.out.println();
                 } else {
-                    System.out.println("Conversion result: " + convertFromDec(targetBase, result));
+                    BigDecimal resultFractionalPart = result.remainder(BigDecimal.ONE);
+                    String fractionalPartStr = "";
+                    if (resultFractionalPart.compareTo(BigDecimal.ZERO) != 0) {
+                        fractionalPartStr = convertFromDecFractionalPart(targetBase, resultFractionalPart);
+                    } else if (!fractionalPart.isEmpty()) {
+                        fractionalPartStr = "00000";
+                    }
+                    System.out.println("Conversion result: " +
+                            convertFromDecIntegerPart(targetBase, result.toBigInteger()) + "." + fractionalPartStr);
                     System.out.println();
                 }
             }
         }
     }
 
-    private static String convertFromDec(int targetBase, BigInteger decimalNumber) {
+    private static String convertFromDecFractionalPart(int targetBase, BigDecimal decimalFractionalPart) {
+        StringBuilder resultSb = new StringBuilder();
+        BigDecimal targetBaseBigDec = BigDecimal.valueOf(targetBase);
+        int precisionCounter = 0;
+        while (!decimalFractionalPart.equals(BigDecimal.ZERO)) {
+            if (precisionCounter >= 5) break;
+            BigDecimal currentNumber = decimalFractionalPart.multiply(targetBaseBigDec);
+            decimalFractionalPart = currentNumber.remainder(BigDecimal.ONE);
+            BigDecimal currentDigit = currentNumber.subtract(decimalFractionalPart);
+            String currentDigitStr = convertFromDecIntegerPart(targetBase, currentDigit.toBigInteger());
+            resultSb.append(currentDigitStr);
+            ++precisionCounter;
+        }
+        if (resultSb.isEmpty()) resultSb.append("0");
+        return resultSb.toString();
+    }
+
+    private static String convertFromDecIntegerPart(int targetBase, BigInteger decimalNumber) {
         StringBuilder resultSb = new StringBuilder();
         BigInteger targetBaseBigInt = BigInteger.valueOf(targetBase);
         while (!decimalNumber.equals(BigInteger.ZERO)) {
